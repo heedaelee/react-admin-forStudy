9878e33c4c2359ed4e946b9501f8a32d9a9b6a96
heedaelee <adguy72@gmail.com>
2022-03-31 15:56:47 +0900

    3/31 view a little bit complete

--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -1,6 +1,58 @@
 import React from "react";
 import "./widgetSm.css";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 export default function WidgetSm() {
-  return <div className="widgetSm">Small</div>;
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Member</span>
+      <ul className="widgetSmList">
+        <li className="widgetSmListItem">
+          <img
+            src="https://api.lorem.space/image/face?w=50&h=50"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <VisibilityIcon className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://api.lorem.space/image/face?w=50&h=50"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <VisibilityIcon className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://api.lorem.space/image/face?w=50&h=50"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <VisibilityIcon className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
