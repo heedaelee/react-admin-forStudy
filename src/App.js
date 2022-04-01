c1b85e7a9de2ab58878f97ffa1fad09bae9074dd
heedaelee <adguy72@gmail.com>
2022-04-01 12:27:32 +0900

    4/1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
+import UserList from "./pages/home/userList/UserList";
 import "./app.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/users" element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/pages/home/userList/UserList.jsx
+++ src/pages/home/userList/UserList.jsx
@@ -1,6 +1,64 @@
-import React from "react";
+import * as React from "react";
 import "./userList.css";
+import { DataGrid } from "@mui/x-data-grid";
+import { DeleteOutline } from "@mui/icons-material";
+import { userRows } from "../userList/dummyData";
+import { Link } from "react-router-dom";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 70 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="userListUser">
+          <img className="userListImg" src={params.row.avatar} alt="" />
+          {params.row.username}
+        </div>
+      );
+    },
+  },
+  { field: "email", headerName: "Email", width: 200 },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 110,
+  },
+  {
+    field: "transaction",
+    headerName: "Transaction",
+    width: 130,
+  },
+  {
+    field: "action",
+    headerName: "Action",
+    width: 150,
+    renderCell: (params) => {
+      return (
+        <>
+          <Link to={"/user/" + params.row.id}>
+            <button className="userListEdit">Edit</button>
+          </Link>
+          <DeleteOutline className="userListDelete" />
+        </>
+      );
+    },
+  },
+];
 
 export default function UserList() {
-  return <div className="userList"></div>;
+  return (
+    <div className="userList">
+      <DataGrid
+        rows={userRows}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  );
 }
